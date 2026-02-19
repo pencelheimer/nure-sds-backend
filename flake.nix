e008239c3014dd81c3180d3f6a3c57e735efcb0d
pencelheimer <pencelheimer@proton.me>
2026-02-19 23:55:01 +0200

    fix!(nix): updated flake.nix to automatically detect OCI backend

--- flake.nix
+++ flake.nix
@@ -1,61 +1,89 @@
 {
   description = "Containerized Backend for the FlashDeck";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
-    nixosModules.default = {
-      config,
-      pkgs,
-      lib,
-      ...
-    }: let
+  outputs = { self, nixpkgs }: {
+    nixosModules.default = { config, pkgs, lib, ... }:
+    let
       cfg = config.services.flashdeck;
+
+      backends = [
+        { name = "user-defined";  value = cfg.backend;  cond = cfg.backend != null; }
+        { name = "docker";        value = "docker";     cond = config.virtualisation.docker.enable; }
+        { name = "podman";        value = "podman";     cond = config.virtualisation.podman.enable; }
+      ];
+
+      effectiveBackend = (lib.findFirst (x: x.cond) { name = "none"; value = "none"; } backends).value;
+
+      backendData = with pkgs; {
+        docker = {
+          bin = "${docker-compose}/bin/docker-compose";
+          deps = [ "docker.service" ];
+          packages = [ docker docker-compose ];
+        };
+        podman = {
+          bin = "${podman-compose}/bin/podman-compose";
+          deps = [ "podman.socket" ];
+          packages = [ podman podman-compose ];
+        };
+        none = { bin = ""; deps = []; packages = []; };
+      }.${effectiveBackend};
+
     in {
       options.services.flashdeck = {
         enable = lib.mkEnableOption "FlashDeck backend service";
+
+        backend = lib.mkOption {
+          type = lib.types.nullOr (lib.types.enum [ "docker" "podman" ]);
+          default = null;
+          description = "Backend to use";
+        };
 
         envVariables = lib.mkOption {
           type = lib.types.attrsOf lib.types.str;
           default = {};
           description = "Env variables for the `.env`";
-          example = {
-            PGRST_JWT_SECRET = "my-secret";
-          };
         };
       };
 
       config = lib.mkIf cfg.enable {
-        virtualisation.podman = {
-          enable = true;
-          dockerCompat = true;
-        };
+        assertions = [{
+          assertion = effectiveBackend != "none";
+          message = ''
+            FlashDeck error: No container backend detected!
+            Please enable docker (virtualisation.docker.enable = true)
+            or podman (virtualisation.podman.enable = true),
+            or set services.flashdeck.backend explicitly.
+          '';
+        }];
 
         systemd.services.flashdeck-backend = {
-          description = "FlashDeck Backend (Pure Nix)";
-          after = ["network-online.target"];
-          wantedBy = ["multi-user.target"];
+          description = "FlashDeck Backend Containers (${effectiveBackend})";
 
-          path = [pkgs.podman pkgs.podman-compose pkgs.coreutils];
+          after = [ "network-online.target" ] ++ backendData.deps;
+          requires = backendData.deps;
+          wantedBy = [ "multi-user.target" ];
+
+          path = with pkgs; [ coreutils ] ++ backendData.packages;
 
           environment = cfg.envVariables;
 
           script = ''
+            set -e
             cd ${self.outPath}
-            podman-compose up -d
+            echo "Starting FlashDeck via ${effectiveBackend} from Nix Store..."
+            ${backendData.bin} up -d
           '';
 
           preStop = ''
             cd ${self.outPath}
-            podman-compose down
+            ${backendData.bin} down
           '';
 
           serviceConfig = {
             Type = "oneshot";
             RemainAfterExit = true;
-            WorkingDirectory = "/var/lib/flash-deck";
             StateDirectory = "flash-deck";
+            WorkingDirectory = "/var/lib/flash-deck";
           };
         };
       };
